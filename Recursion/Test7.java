58db07f772bfb8943fee5223a0d10215f730c28b
Aniket Patil <patilaniket7757@gmail.com>
2025-09-27 22:19:49 +0530

    Add recursive functions in Test6 and Test7 classes

---
+++ Recursion/Test7.java
@@ -0,0 +1,16 @@
+public class Test7 {
+    static void fun(int n) {
+        if (n == 0) {
+            return;
+        }
+         fun(n - 1);
+        System.out.println(n);
+        fun(n - 1);
+       
+    }
+
+    public static void main(String[] args) {
+        fun(3);
+    }
+  
+}
